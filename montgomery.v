b6055ece75b7c69646122b06182062c70b44ac04
wvangansbeke <woutervangansbeke@hotmail.com>
2019-01-30 09:05:00 +0100

    Update montgomery.v

--- montgomery.v
+++ montgomery.v
@@ -11,15 +11,6 @@ module montgomery(
     output done
      );
  
-    /*
-    Student tasks:
-    1. Instantiate an Adder
-    2. Use the Adder to implement the Montgomery multiplier in hardware.
-    3. Use tb_montgomery.v to simulate your design.
-    */
-    
-    //This always block was added to ensure the tool doesn't trim away the montgomery module.
-    //Students: Feel free to remove this block
     reg [1023:0] A, B, M, add_a, add_b;
     reg [10:0] n;
     reg [1025:0] r_result, C, D;
